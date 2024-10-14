28e107b9d91fff431ad253cf797a757b8a8a7f07
Abraham Jose <abrahamjose002@gmail.com>
2024-10-14 10:21:57 +0530

    axios changed

--- src/axios/axiosInstance.tsx
+++ src/axios/axiosInstance.tsx
@@ -1,14 +1,11 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL:  'https://stock-image-backend.onrender.com/api',
-  withCredentials: true,
-});
-
-axiosInstance.interceptors.request.use((config) => {
-  return config;
-}, (error) => {
-  return Promise.reject(error);
+  baseURL: process.env.NODE_ENV === 'production' 
+    ? 'https://stock-image-backend.onrender.com/api' 
+    : 'http://localhost:10000/api',
+  withCredentials: true,  
 });
 
 export default axiosInstance;
+
